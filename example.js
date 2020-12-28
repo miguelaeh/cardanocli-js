b8c469499f1c25030078af434a9bf50c62969807
Alessandro <akonrad6@gmail.com>
2020-12-28 16:05:19 +0100

    added new functionality

--- example.js
+++ example.js
@@ -1,17 +1,49 @@
 const CardanoJs = require("./index.js");
+const os = require("os");
+const path = require("path");
 
-const cardanoJs = new CardanoJs({ era: "allegra" });
+const shelleyPath = "/opt/cardano/pi-core/files/mainnet-shelley-genesis.json";
 
-// console.log(cardanoJs.queryUtxo("Ae2tdPwUPEYwNguM7TB3dMnZMfZxn1pjGHyGdjaF4mFqZF9L3bj6cdhiH8t"));
+const cardanoJs = new CardanoJs({
+  era: "allegra",
+  shelleyGenesisPath: shelleyPath,
+});
 
-const createWallet = accout => {
-    cardanoJs.addressKeyGen(accout);
-    cardanoJs.stakeAddressKeyGen(accout);
-    cardanoJs.stakeAddressBuild(accout);
-    cardanoJs.addressBuild(accout);
-    return [cardanoJs.address(accout), cardanoJs.stakingAddress(accout)];
-}
+//query utxo of an address
+console.log(
+  cardanoJs.queryUtxo(
+    "Ae2tdPwUPEYwNguM7TB3dMnZMfZxn1pjGHyGdjaF4mFqZF9L3bj6cdhiH8t"
+  )
+);
 
-let wallet = createWallet("Wow");
+//create a wallet
 
-console.log(wallet);
+// const createWallet = (accout) => {
+//   cardanoJs.addressKeyGen(accout);
+//   cardanoJs.stakeAddressKeyGen(accout);
+//   cardanoJs.stakeAddressBuild(accout);
+//   cardanoJs.addressBuild(accout);
+// };
+
+// createWallet("Wow");
+
+//read out wallet info
+console.log(cardanoJs.wallet("Wow").summary);
+//read out wallet file content
+console.log(cardanoJs.wallet("Wow").file("payment.vkey"));
+
+//create a pool
+// const createPool = (pool) => {
+//   cardanoJs.nodeKeyGenKES(pool);
+//   cardanoJs.nodeKeyGen(pool);
+//   cardanoJs.nodeIssueOpCert(pool);
+//   cardanoJs.nodeKeyGenVRF(pool);
+// };
+
+// createPool("Test");
+
+//pool meta hash
+// const poolmeta =
+//   '{\n  "name": "Berry",\n  "ticker": "BERRY",\n  "description": "Decentralizing Cardano in the most beautiful way - Let\'s shape the future together!",\n  "homepage": "https://pipool.online",\n  "extended": "https://github.com/alessandrokonrad/Pi-Pool/raw/master/meta/extended_poolmeta.json"\n}\n';
+
+// console.log(cardanoJs.stakePoolMetadataHash(poolmeta));
--- index.js
+++ index.js
@@ -1,150 +1,275 @@
 const execSync = require("child_process").execSync;
+const fs = require("fs");
 
 class CardanoJs {
-    /**
-     *
-     * @param {JSON} options - {socketPath: optional, era:optional, network: optional, dir: optional}
-     */
+  /**
+   *
+   * @param {JSON} options - {shelleyGenesisPath: required, socketPath: optional, era:optional, network: optional, dir: optional}
+   */
 
-    constructor(options) {
-        this.network = `--mainnet`;
-        this.era = "";
-        this.dir = ".";
-        if (options) {
-            options.socketPath &&
-                execSync(`export CARDANO_NODE_SOCKET_PATH=${options.socketPath}`);
-            options.era && (this.era = "--" + options.era + "-era");
-            options.network && (this.network = "--" + options.network);
-            options.dir && (this.dir = options.dir);
-        }
-        this.protocolParameters = this.queryProtcolParameters();
-    }
+  constructor(options) {
+    this.network = `--mainnet`;
+    this.era = "";
+    this.dir = ".";
+    if (!options.shelleyGenesisPath)
+      throw new Error("shelleyGenesisPath required");
+    this.shelleyGenesis = JSON.parse(
+      execSync(`cat ${options.shelleyGenesisPath}`).toString()
+    );
 
-    queryProtcolParameters() {
-        execSync(`mkdir -p ${this.dir}/tmp`)
-        execSync(`cardano-cli query protocol-parameters \
+    options.socketPath &&
+      execSync(`export CARDANO_NODE_SOCKET_PATH=${options.socketPath}`);
+    options.era && (this.era = "--" + options.era + "-era");
+    options.network && (this.network = "--" + options.network);
+    options.dir && (this.dir = options.dir);
+
+    execSync(`mkdir -p ${this.dir}/tmp`);
+    this.protocolParameters = this.queryProtcolParameters();
+  }
+
+  queryProtcolParameters() {
+    execSync(`cardano-cli query protocol-parameters \
                             ${this.network} \
                             --cardano-mode \
                             --out-file ${this.dir}/tmp/protocolParams.json \
                             ${this.era}
-                        `)
-        return JSON.parse(
-            execSync(`cat ${this.dir}/tmp/protocolParams.json`))
-    }
+                        `);
+    return JSON.parse(execSync(`cat ${this.dir}/tmp/protocolParams.json`));
+  }
 
-    queryTip() {
-        return JSON.parse(
-            execSync(`cardano-cli query tip \
+  queryTip() {
+    return JSON.parse(
+      execSync(`cardano-cli query tip \
         ${this.network} \
         --cardano-mode
                         `).toString()
-        );
-    }
+    );
+  }
 
-    /**
-     * 
-     * @param {string} address - Staking address 
-     */
-    queryStakeAddressInfo(address) {
-        return JSON.parse(
-            execSync(`cardano-cli query stake-address-info \
+  /**
+   *
+   * @param {string} address - Staking address
+   */
+  queryStakeAddressInfo(address) {
+    return JSON.parse(
+      execSync(`cardano-cli query stake-address-info \
         ${this.network} \
         --address ${address} \
         ${this.era}
         `).toString()
-        );
-    }
+    );
+  }
 
-    /**
-     * 
-     * @param {string} address - Payment address 
-     */
-    queryUtxo(address) {
-        let utxosRaw = execSync(`cardano-cli query utxo \
+  /**
+   *
+   * @param {string} address - Payment address
+   */
+  queryUtxo(address) {
+    let utxosRaw = execSync(`cardano-cli query utxo \
             ${this.network} \
             --address ${address} \
             --cardano-mode \
             ${this.era}
-            `).toString()
+            `).toString();
 
-        let utxos = utxosRaw.split('\n');
-        utxos.splice(0, 1);
-        utxos.splice(0, 1);
-        utxos.splice(utxos.length - 1, 1);
-        let result = utxos.map((raw, index) => {
-            let utxo = raw.replace(/\s+/g, " ").split(" ");
-            return { txHash: utxo[0], txId: parseInt(utxo[1]), amount: parseInt(utxo[2]) }
-        })
+    let utxos = utxosRaw.split("\n");
+    utxos.splice(0, 1);
+    utxos.splice(0, 1);
+    utxos.splice(utxos.length - 1, 1);
+    let result = utxos.map((raw, index) => {
+      let utxo = raw.replace(/\s+/g, " ").split(" ");
+      return {
+        txHash: utxo[0],
+        txId: parseInt(utxo[1]),
+        amount: parseInt(utxo[2]),
+      };
+    });
 
+    return result;
+  }
 
-        return result;
-    }
-
-    /**
-     * 
-     * @param {string} account - Name for the payment account keys 
-     */
-    addressKeyGen(account) {
-        execSync(`mkdir -p ${this.dir}/priv/wallet/${account}`)
-        execSync(`cardano-cli address key-gen \
+  /**
+   *
+   * @param {string} account - Name for the payment account keys
+   */
+  addressKeyGen(account) {
+    execSync(`mkdir -p ${this.dir}/priv/wallet/${account}`);
+    execSync(`cardano-cli address key-gen \
                         --verification-key-file ${this.dir}/priv/wallet/${account}/${account}.payment.vkey \
                         --signing-key-file ${this.dir}/priv/wallet/${account}/${account}.payment.skey
-                    `)
-    }
+                    `);
+  }
 
-    /**
-     * 
-     * @param {string} account - Name for the stake account keys 
-     */
-    stakeAddressKeyGen(account) {
-        execSync(`mkdir -p ${this.dir}/priv/wallet/${account}`)
-        execSync(`cardano-cli stake-address key-gen \
+  /**
+   *
+   * @param {string} account - Name for the stake account keys
+   */
+  stakeAddressKeyGen(account) {
+    execSync(`mkdir -p ${this.dir}/priv/wallet/${account}`);
+    execSync(`cardano-cli stake-address key-gen \
                         --verification-key-file ${this.dir}/priv/wallet/${account}/${account}.stake.vkey \
                         --signing-key-file ${this.dir}/priv/wallet/${account}/${account}.stake.skey
-                    `)
-    }
+                    `);
+  }
 
-    /**
-   * 
+  /**
+   *
    * @param {string} account - Name for the account
    */
-    stakeAddressBuild(account) {
-        execSync(`cardano-cli stake-address build \
+  stakeAddressBuild(account) {
+    execSync(`cardano-cli stake-address build \
                         --staking-verification-key-file ${this.dir}/priv/wallet/${account}/${account}.stake.vkey \
                         --out-file ${this.dir}/priv/wallet/${account}/${account}.stake.addr \
-                        --mainnet
-                    `)
-    }
+                        ${this.network}
+                    `);
+  }
 
-    /**
- * 
- * @param {string} account - Name for the account
- */
-    addressBuild(account) {
-        execSync(`cardano-cli address build \
+  /**
+   *
+   * @param {string} account - Name for the account
+   */
+  addressBuild(account) {
+    execSync(`cardano-cli address build \
                     --payment-verification-key-file ${this.dir}/priv/wallet/${account}/${account}.payment.vkey \
                     --staking-verification-key-file ${this.dir}/priv/wallet/${account}/${account}.stake.vkey \
                     --out-file ${this.dir}/priv/wallet/${account}/${account}.payment.addr \
-                    --mainnet
-                `)
-    }
+                    ${this.network}
+                `);
+  }
 
-    address(account) {
-        return execSync(`cat ${this.dir}/priv/wallet/${account}/${account}.payment.addr`).toString();
-    }
+  wallet(account) {
+    const paymentAddr = fs
+      .readFileSync(
+        `${this.dir}/priv/wallet/${account}/${account}.payment.addr`
+      )
+      .toString();
+    const stakingAddr = fs
+      .readFileSync(`${this.dir}/priv/wallet/${account}/${account}.stake.addr`)
+      .toString();
 
-    stakingAddress(account) {
-        return execSync(`cat ${this.dir}/priv/wallet/${account}/${account}.stake.addr`).toString();
-    }
+    const balance = this.queryUtxo(paymentAddr).reduce(
+      (acc, curr) => acc + curr.amount,
+      0
+    );
 
-    stakeAddressRegistrationCertificate(account) {
-        execSync(`cardano-cli stake-address registration-certificate \
+    let reward = this.queryStakeAddressInfo(stakingAddr);
+    reward = reward.find((delegation) => delegation.address == stakingAddr)
+      ? /**
+         *
+         * @param {string} account - Name for the account
+         */
+        reward.find((delegation) => delegation.address == stakingAddr)
+          .rewardAccountBalance
+      : "Staking key not registered";
+
+    return {
+      summary: {
+        paymentAddr,
+        stakingAddr,
+        balance,
+        reward,
+      },
+      file: (fileName) => {
+        let content;
+        try {
+          content = fs
+            .readFileSync(
+              `${this.dir}/priv/wallet/${account}/${account}.${fileName}`
+            )
+            .toString();
+        } catch (err) {
+          throw new Error(`File ${file} of Account ${account} doesn't exist`);
+        }
+        return {
+          path: `${this.dir}/priv/wallet/${account}/${account}.${fileName}`,
+          content,
+        };
+      },
+    };
+  }
+
+  stakeAddressRegistrationCertificate(account) {
+    execSync(`cardano-cli stake-address registration-certificate \
                         --staking-verification-key-file ${this.dir}/priv/wallet/${account}/${account}.stake.vkey \
                         --out-file ${this.dir}/priv/wallet/${account}/${account}.stake.cert
-                    `)
-    }
+                    `);
+  }
 
+  /**
+   *
+   * @param {string} pool - Pool name
+   */
+  nodeKeyGenKES(pool) {
+    execSync(`mkdir -p ${this.dir}/priv/pool/${pool}`);
+    execSync(`cardano-cli node key-gen-KES \
+                        --verification-key-file ${this.dir}/priv/pool/${pool}/${pool}.kes.vkey \
+                        --signing-key-file ${this.dir}/priv/pool/${pool}/${pool}.kes.skey
+                    `);
+  }
+
+  nodeKeyGen(pool) {
+    execSync(`mkdir -p ${this.dir}/priv/pool/${pool}`);
+    execSync(`cardano-cli node key-gen \
+                        --cold-verification-key-file ${this.dir}/priv/pool/${pool}/${pool}.node.vkey \
+                        --cold-signing-key-file ${this.dir}/priv/pool/${pool}/${pool}.node.skey \
+                        --operational-certificate-issue-counter ${this.dir}/priv/pool/${pool}/${pool}.node.counter 
+                    `);
+  }
+
+  nodeIssueOpCert(pool) {
+    execSync(`cardano-cli node issue-op-cert \
+                        --kes-verification-key-file ${
+                          this.dir
+                        }/priv/pool/${pool}/${pool}.kes.vkey \
+                        --cold-signing-key-file ${
+                          this.dir
+                        }/priv/pool/${pool}/${pool}.node.skey \
+                        --operational-certificate-issue-counter ${
+                          this.dir
+                        }/priv/pool/${pool}/${pool}.node.counter \
+                        --kes-period ${this.KESPeriod()} \
+                        --out-file ${
+                          this.dir
+                        }/priv/pool/${pool}/${pool}.node.cert 
+                    `);
+  }
+
+  nodeKeyGenVRF(pool) {
+    execSync(`mkdir -p ${this.dir}/priv/pool/${pool}`);
+    execSync(`cardano-cli node key-gen-VRF \
+                        --verification-key-file ${this.dir}/priv/pool/${pool}/${pool}.vrf.vkey \
+                        --signing-key-file ${this.dir}/priv/pool/${pool}/${pool}.vrf.skey
+                    `);
+  }
+
+  stakePoolId(pool) {
+    return execSync(
+      `cardano-cli stake-pool id --cold-verification-key-file ${this.dir}/priv/pool/${pool}/${pool}.node.vkey`
+    )
+      .toString()
+      .replace(/\s+/g, " ");
+  }
+
+  /**
+   *
+   * @param {string} metadata | original file content
+   */
+  stakePoolMetadataHash(metadata) {
+    fs.writeFileSync(`${this.dir}/tmp/poolmeta.json`, metadata);
+    let metaHash = execSync(
+      `cardano-cli stake-pool metadata-hash --pool-metadata-file ${this.dir}/tmp/poolmeta.json`
+    )
+      .toString()
+      .replace(/\s+/g, " ");
+    execSync(`rm ${this.dir}/tmp/poolmeta.json`);
+    return metaHash;
+  }
+
+  KESPeriod() {
+    return parseInt(
+      this.queryTip().slotNo / this.shelleyGenesis.slotsPerKESPeriod
+    );
+  }
 }
-
 
 module.exports = CardanoJs;
