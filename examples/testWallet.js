b0a38aa7d3893a0f067f966bbe6fb4d36332ef39
alessandrokonrad <akonrad6@gmail.com>
2020-12-31 11:04:26 +0000

    added Beta branch

---
+++ examples/testWallet.js
@@ -0,0 +1,21 @@
+const CardanocliJs = require("../index.js");
+const os = require("os");
+const path = require("path");
+
+const dir = path.join(os.homedir(), "testnet");
+const shelleyPath = path.join(
+  os.homedir(),
+  "testnet",
+  "testnet-shelley-genesis.json"
+);
+
+const cardanocliJs = new CardanocliJs({
+  era: "allegra",
+  network: "testnet-magic 1097911063",
+  dir: dir,
+  shelleyGenesisPath: shelleyPath,
+});
+
+let wallet = cardanocliJs.wallet("Ales");
+
+console.log(wallet);
